cdb7c3be2daf5e9805c8072519555afdf96da333
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-05-30 11:45:35 -0600

    initialize next.js and basic infrastructure for express

---
+++ express-server/src/server.ts
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (_req, res) => {
+    res.send("Hello World!");
+})
+
+app.get('/user/:id', (req, res) => {
+    let dummyData = {
+        "id": req.params["id"],
+        "username": "Ash",
+        "wins": 15,
+        "losses": 10,
+    }
+    res.json(dummyData)
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
